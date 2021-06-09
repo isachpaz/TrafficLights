bb0ea15391fadd621cd6774ffd0e3692c3ce1fe2
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2021-06-09 17:30:20 +0200

    TrafficLightStatus and TrafficLight class added

--- TrafficLightsSolution/TrafficLightsLib/TrafficLight.cs
+++ TrafficLightsSolution/TrafficLightsLib/TrafficLight.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IntervalLib;
 
 namespace TrafficLightsLib
 {
     public class TrafficLight
     {
         private readonly double _cutoff;
-
+        
         public TrafficLight(double cutoff)
         {
             _cutoff = cutoff;
+
+            //Dictionary<TrafficLightStatus, Func<Interval<>, bool>>
         }
 
         public TrafficLightStatus CheckValue(double value)
